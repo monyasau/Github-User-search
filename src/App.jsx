8f5dbfb6e2c4c4dfdaae2a61105d34625aa6ebc4
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-31 13:02:28 +0100

    Updates: added nav bar an input box for user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import GithubSearch from './components/GithubSearch'
+import TopNav from './components/TopNav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <TopNav/>
+      <GithubSearch/>
     </>
   )
 }
---
+++ src/components/GithubSearch.jsx
@@ -0,0 +1,15 @@
+let GithubSearch = () => {
+  return (
+    <>
+      <div className="">
+        <div className="flex max-w-screen-xl w-full mx-auto justify-between py-10 md:px-0 px-4">
+          <input type="text" className="border  w-full mx-auto"/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+// api: https://api.github.com/search/users?q=ogbenihmmd
+
+export default GithubSearch;
