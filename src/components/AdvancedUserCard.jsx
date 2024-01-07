3be965076023b3435caf84eaf7b3e00e01e45ce6
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-01-07 10:19:28 +0100

    Added an advanced user info component

---
+++ src/components/AdvancedUserCard.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const AdvancedUserCard=({userInfo})=> {
+    return (
+        <div>
+    
+        </div>
+    );
+}
+
+export default AdvancedUserCard;
+// "url": "https://api.github.com/users/440",
