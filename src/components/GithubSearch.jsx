7d8cb9ea3133bb1856db942804b3d0b706e4ee15
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-01-08 19:46:15 +0100

    disabled  api header due to  consistent api key issue

--- src/components/GithubSearch.jsx
+++ src/components/GithubSearch.jsx
@@ -13,9 +13,9 @@ const fetchGithubData=()=>{
   setLoading(true);
   axios
   .get(`https://api.github.com/search/users?q=${userInput}`, {
-    headers: {
-      Authorization: `Bearer ${githubToken}`,
-    },
+    // headers: {
+    //   Authorization: `Bearer ${githubToken}`,
+    // },
   })
   .then((response) => {
     setUserData(response.data);
@@ -45,7 +45,12 @@ const fetchGithubData=()=>{
             className="border flex md:h-[70px] h-[50px] rounded md:rounded-none p-4 items-center w-full mx-auto"
           >
             <input type="text" autoFocus className=" focus:outline-none flex-grow " onKeyUp={updateInput} />
-            {loading===false?
+            {loading?
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className=" animate-spin" viewBox="0 0 16 16">
+              <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
+              <path d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
+            </svg>
+              :
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -53,17 +58,14 @@ const fetchGithubData=()=>{
                 strokeWidth={1.5}
                 stroke="#b9b9b9"
                 className="w-6 mx-auto h-6"
+                onClick={fetchGithubData}
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                 />
-              </svg>:
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className=" animate-spin" viewBox="0 0 16 16">
-              <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
-              <path d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
-            </svg>
+              </svg>
               }
           </div>
           <div
