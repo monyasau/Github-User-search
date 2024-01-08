2da040eb5762a4e68d5b0a8f9829da2ced5a69d4
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-01-08 19:46:47 +0100

    update: fix overflow issue and others

--- src/components/AdvancedUserCard.jsx
+++ src/components/AdvancedUserCard.jsx
@@ -11,10 +11,13 @@ const AdvancedUserCard = ({ userInfo }) => {
             id="backdrop"
             className=" fixed inset-0 z-40 bg-black/20 backdrop-blur dark:bg-slate-900/80"
             ></div>
-          <div className="fixed  top-[5vh] mx-auto left-0  z-50  w-full flex flex-col items-center">
+          <div className="fixed  md:top-1/3 top-[5vh] max-h-[50vh]  mx-auto left-0  z-50  w-full flex flex-col items-center">
 
 
-            <div className="md:w-[500px] w-[90vw] bg-white h-500px p-4 rounded">
+            <div className="md:w-[500px] w-[90vw] bg-white overflow-y-scroll p-4 rounded">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit facere assumenda facilis, debitis iste alias nihil iusto nam, quasi doloremque quidem rerum quod doloribus ipsum. Maxime placeat facere laboriosam culpa!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, placeat expedita. Quisquam error dignissimos officiis quae porro? Corporis, saepe dolorum placeat nemo veritatis asperiores voluptatum, facilis beatae, ipsum eveniet mollitia.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur veritatis dolorum architecto similique harum maxime ut ab iste amet, quaerat, doloribus ad officia pariatur quidem obcaecati? Cum delectus aut incidunt.
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit facere assumenda facilis, debitis iste alias nihil iusto nam, quasi doloremque quidem rerum quod doloribus ipsum. Maxime placeat facere laboriosam culpa!
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, placeat expedita. Quisquam error dignissimos officiis quae porro? Corporis, saepe dolorum placeat nemo veritatis asperiores voluptatum, facilis beatae, ipsum eveniet mollitia.
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur veritatis dolorum architecto similique harum maxime ut ab iste amet, quaerat, doloribus ad officia pariatur quidem obcaecati? Cum delectus aut incidunt.
